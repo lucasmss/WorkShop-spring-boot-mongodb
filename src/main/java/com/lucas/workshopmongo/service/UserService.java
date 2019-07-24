7cbae474322f808fa0cf23b12f1afaf96e39e6dd
Lucas Mateus <lucasmss16@gmail.com>
2019-07-24 14:47:32 -0300

    Correcçaõ do erro e addd porta 404

--- src/main/java/com/lucas/workshopmongo/service/UserService.java
+++ src/main/java/com/lucas/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.lucas.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucas.workshopmongo.domain.User;
 import com.lucas.workshopmongo.repository.UserRepository;
+import com.lucas.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
 		return  repo.findAll();
 		
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 	
 }
