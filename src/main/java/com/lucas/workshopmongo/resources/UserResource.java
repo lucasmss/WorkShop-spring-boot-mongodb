83a727792c4945edf1a3dd0666007358c2025c00
Lucas Mateus <lucasmss16@gmail.com>
2019-07-26 17:13:48 -0300

    Atualização update

--- src/main/java/com/lucas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucas/workshopmongo/resources/UserResource.java
@@ -52,5 +52,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.Update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 }
