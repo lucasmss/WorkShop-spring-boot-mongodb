59d5cce6be3ec45c2d7670d5dfe847144d4b4241
Lucas Mateus <lucasmss16@gmail.com>
2019-07-29 14:45:39 -0300

    Atualização update

--- src/main/java/com/lucas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucas/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.lucas.workshopmongo.domain.Post;
 import com.lucas.workshopmongo.domain.User;
+import com.lucas.workshopmongo.dto.AuthorDTO;
 import com.lucas.workshopmongo.repository.PostRepository;
 import com.lucas.workshopmongo.repository.UserRepository;
 
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços",maria);
-		Post post2 = new Post(null, sdf.parse("30/03/2019"), "Bom Dia", "Paris, tamo aqui!!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("30/03/2019"), "Bom Dia", "Paris, tamo aqui!!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
