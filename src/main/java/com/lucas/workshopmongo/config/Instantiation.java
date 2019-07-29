2cce98bf8308f61096b5f4bca094782fbefaf5ed
Lucas Mateus <lucasmss16@gmail.com>
2019-07-29 15:53:44 -0300

    Colocou usuarios ao posts

--- src/main/java/com/lucas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucas/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lucas.workshopmongo.domain.Post;
 import com.lucas.workshopmongo.domain.User;
 import com.lucas.workshopmongo.dto.AuthorDTO;
+import com.lucas.workshopmongo.dto.CommentDTO;
 import com.lucas.workshopmongo.repository.PostRepository;
 import com.lucas.workshopmongo.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("30/03/2019"), "Bom Dia", "Paris, tamo aqui!!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem mano!", sdf.parse("21/03/2019"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/09/2019"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/06/2019"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post1.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
