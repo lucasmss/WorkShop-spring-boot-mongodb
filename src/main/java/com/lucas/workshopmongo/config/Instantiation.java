ecd41f985dc8810838b8357b5b33cabeec9db071
Lucas Mateus <lucasmss16@gmail.com>
2019-07-29 15:01:40 -0300

    Referençia dos post

--- src/main/java/com/lucas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucas/workshopmongo/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("30/03/2019"), "Bom Dia", "Paris, tamo aqui!!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
